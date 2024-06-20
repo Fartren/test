974ab302c8cdc717c287aae246a5ea57f195e169
Baptiste Vincent <bvincent@RSSI-TESTS.local>
2024-06-20 14:17:41 +0200

    scan2

--- source_lang.php
+++ source_lang.php
@@ -1,7 +1,21 @@
 <html>
 <body>
 <?php
-scandir('./');
+$arrFiles = array();
+$dirPath = "./MyFolder";
+
+
+// Using scandir()
+echo "--------------------------------<br>";
+echo "Method 1: Using scandir() <br>";
+echo "--------------------------------<br>";
+$files = scandir($dirPath);
+foreach ($files as $file) {
+    $filePath = $dirPath . '/' . $file;
+    if (is_file($filePath)) {
+        echo $file . "<br>";
+    }
+}
 ?>
 </body>
 </html>
